8eb0f44facfe00fbc709bea0fbce6f7795d9b528
Justin Jakel <jjakel@mantech-inc.com>
2025-07-20 16:38:10 -0400

    Added config suppression

---
+++ Soundboard/Soundboard.Services/GlobalSuppressions.cs
@@ -0,0 +1,13 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "<Pending>", Scope = "member", Target = "~M:Soundboard.Services.GlobalHotkeyService.HotkeyMessageWindow.#ctor(System.Action{System.Int32})")]
+[assembly: SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "<Pending>", Scope = "member", Target = "~M:Soundboard.Services.GlobalHotkeyService.HotkeyMessageWindow.Dispose")]
+[assembly: SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "<Pending>", Scope = "member", Target = "~M:Soundboard.Services.GlobalHotkeyService.HotkeyMessageWindow.WndProc(System.Windows.Forms.Message@)")]
+[assembly: SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "<Pending>", Scope = "member", Target = "~M:Soundboard.Services.GlobalHotkeyService.RegisterHotkey(System.Int32,System.Windows.Input.ModifierKeys,System.Windows.Forms.Keys,System.Action)~System.Boolean")]
+[assembly: SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "<Pending>", Scope = "member", Target = "~M:Soundboard.Services.GlobalHotkeyService.UnregisterAllHotkeys")]
+[assembly: SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "<Pending>", Scope = "member", Target = "~M:Soundboard.Services.GlobalHotkeyService.UnregisterHotkey(System.Int32)")]
