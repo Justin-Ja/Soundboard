031256ba64dd7088aea3354b88aa0958f26d8cd2
Justin Jakel <jjakel@mantech-inc.com>
2025-07-27 16:46:52 -0400

    Completed TODOs related to mainViewModel

---
+++ Soundboard/Soundboard/MainViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using System.Windows.Input;
+using Soundboard.Common;
+
+namespace Soundboard;
+
+public class MainViewModel : BaseViewModel
+{
+    //When more components are added to the main view and not the button grid, this will be used again, or delegate again
+    //But for now it remains empty
+    public MainViewModel()
+    {
+    }
+}
+
+
