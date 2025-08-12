9172c9d11c81903497fab45bed36722d87ae7780
Justin Jakel <yustinyakel100@gmail.com>
2025-08-12 18:30:16 -0400

    Updated packages

---
+++ Soundboard/Soundboard.Domain.DataAccess/Implementations/ISoundboardRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soundboard.Domain.DataAccess.Implementations;
+
+public interface ISoundboardRepository
+{
+    Task<List<SoundButton>> GetAllSoundButtonsAsync();
+    Task<List<SoundButtonGridLayout>> GetAllCategoriesAsync();
+    Task<SoundButton> AddSoundButtonAsync(SoundButton soundButton);
+    Task<SoundButtonGridLayout> AddCategoryAsync(SoundButtonGridLayout category);
+    Task UpdateSoundButtonAsync(SoundButton soundButton);
+    Task DeleteSoundButtonAsync(Guid id);
+    Task<SoundButton> GetSoundButtonByIdAsync(Guid id);
+}
+
---
+++ Soundboard/Soundboard.Domain.DataAccess/Implementations/SoundboardRepository.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Soundboard.Domain.DataAccess.Implementations;
+
+
+public class SoundboardRepository : ISoundboardRepository
+{
+    private readonly SoundboardDbContext _context;
+
+    public SoundboardRepository(SoundboardDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<SoundButton>> GetAllSoundButtonsAsync()
+    {
+        return await _context.SoundButtons
+            .Include(sb => sb.GridLayout)
+            .ToListAsync();
+    }
+
+    public async Task<List<SoundButtonGridLayout>> GetAllCategoriesAsync()
+    {
+        return await _context.GridLayouts
+            .Include(c => c.SoundButtons)
+            .ToListAsync();
+    }
+
+    public async Task<SoundButton> AddSoundButtonAsync(SoundButton soundButton)
+    {
+        _context.SoundButtons.Add(soundButton);
+        await _context.SaveChangesAsync();
+        return soundButton;
+    }
+
+    public async Task<SoundButtonGridLayout> AddCategoryAsync(SoundButtonGridLayout gridLayout)
+    {
+        _context.GridLayouts.Add(gridLayout);
+        await _context.SaveChangesAsync();
+        return gridLayout;
+    }
+
+    public async Task UpdateSoundButtonAsync(SoundButton soundButton)
+    {
+        _context.SoundButtons.Update(soundButton);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteSoundButtonAsync(Guid id)
+    {
+        var soundButton = await _context.SoundButtons.FindAsync(id);
+        if (soundButton != null)
+        {
+            _context.SoundButtons.Remove(soundButton);
+            await _context.SaveChangesAsync();
+        }
+    }
+
+    public async Task<SoundButton> GetSoundButtonByIdAsync(Guid id)
+    {
+        return await _context.SoundButtons
+            .Include(sb => sb.GridLayout)
+            .FirstAsync();
+    }
+}
+
