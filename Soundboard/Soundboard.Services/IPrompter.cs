4c5e79cacd69d721fa356f1bd55ddcd73f217796
Justin Jakel <yustinyakel100@gmail.com>
2025-08-16 16:42:58 -0400

    Created a promtper service for the toolbar

---
+++ Soundboard/Soundboard.Services/IPrompter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Soundboard.Domain.DataAccess.Implementations;
+
+namespace Soundboard.Services;
+
+public interface IPrompter
+{
+    string PromptForText(string title, string message, string defaultText = "");
+    bool PromptForConfirmation(string title, string message);
+    SoundButtonGridLayout PromptForGridSelection(string title, List<SoundButtonGridLayout> availableGrids);
+}
+
+public class Prompter : IPrompter
+{
+    public string PromptForText(string title, string message, string defaultText = "")
+    {
+        var promptWindow = new PromptWindow(title, message, defaultText);
+        var result = promptWindow.ShowDialog();
+
+        return result == true ? promptWindow.ResponseText : null;
+    }
+
+    public bool PromptForConfirmation(string title, string message)
+    {
+        var result = MessageBox.Show(message, title, MessageBoxButton.YesNo, MessageBoxImage.Question);
+        return result == MessageBoxResult.Yes;
+    }
+
+    public SoundButtonGridLayout PromptForGridSelection(string title, List<SoundButtonGridLayout> availableGrids)
+    {
+        var gridSelectionWindow = new GridSelectionWindow(title, availableGrids);
+        var result = gridSelectionWindow.ShowDialog();
+
+        return result == true ? gridSelectionWindow.SelectedGrid : null;
+    }
+}
---
+++ Soundboard/Soundboard/Views/PromptWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Soundboard.Views
+{
+    /// <summary>
+    /// Interaction logic for PromptWindow.xaml
+    /// </summary>
+    public partial class PromptWindow : Window
+    {
+        new public string Title { get; }
+        public string Message { get; }
+        public string ResponseText { get; set; }
+
+        public PromptWindow(string title, string message, string defaultText = "")
+        {
+            InitializeComponent();
+
+            Title = title;
+            Message = message;
+            ResponseText = defaultText;
+
+            DataContext = this;
+
+            Loaded += (s, e) => ResponseTextBox.Focus();
+        }
+
+        private void OkButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(ResponseText))
+            {
+                DialogResult = false;
+            }
+            else
+            {
+                DialogResult = true;
+            }
+            this.Close();
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+            this.Close();
+        }
+    }
+}
